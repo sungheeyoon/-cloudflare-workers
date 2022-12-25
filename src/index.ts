8aff9378b807df03dfb20ba97dba0bb9e2068200
sungheeyoon <torushy@gmail.com>
2022-12-25 23:37:13 +0900

    3.Messages

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,17 @@ export interface Env {
   DB: KVNamespace;
   CHAT: DurableObjectNamespace;
 }
-
 // @ts-ignore
 import home from "./home.html";
 
 export class ChatRoom {
   state: DurableObjectState;
+  users: WebSocket[];
+  messages: string[];
   constructor(state: DurableObjectState, env: Env) {
     this.state = state;
+    this.users = [];
+    this.messages = [];
   }
   handleHome() {
     return new Response(home, {
@@ -30,7 +33,13 @@ export class ChatRoom {
   }
   handleWebSocket(webSocket: WebSocket) {
     webSocket.accept();
+    this.users.push(webSocket);
     webSocket.send(JSON.stringify({ message: "hello from backend!" }));
+    this.messages.forEach((message) => webSocket.send(message));
+    webSocket.addEventListener("message", (event) => {
+      this.messages.push(event.data.toString());
+      this.users.forEach((user) => user.send(event.data));
+    });
   }
   async fetch(request: Request) {
     const { pathname } = new URL(request.url);
